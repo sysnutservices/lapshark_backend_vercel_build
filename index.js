766d54ce0216f0f47dc4ef7e0050a064abe77be9
Your Name <your@email.com>
2025-12-10 13:29:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const serverless = require("serverless-http");
-const appModule = require("./dist/server");
-const app = appModule.default || appModule;
+// const serverless = require("serverless-http");
+// const appModule = require("./dist/server");
+// const app = appModule.default || appModule;
+// module.exports = serverless(app);
 
-module.exports = serverless(app);
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
